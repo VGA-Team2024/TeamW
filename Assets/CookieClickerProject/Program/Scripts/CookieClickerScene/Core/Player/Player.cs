832afe1ad8cc77019b0a7d78c12d42a02fd8ba94
YamamotoU518 <hw37hnmr@gmail.com>
2024-02-07 20:19:27 +0900

    [Add]Playerの作成、Skillの作成

---
+++ Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/Core/Player/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        handleClick();
+    }
+}
