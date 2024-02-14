b21be55a317a5b37acbe853e5732c47c48c3dbf0
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-02-14 17:44:47 +0900

    [Add]MVP

---
+++ Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/UI/ResourceView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+namespace CookieClickerProject.CookieClickerScene.UI
+{
+    public class ResourceView : MonoBehaviour
+    {
+        [SerializeField] private Text totalCookiesText;
+        [SerializeField] private Button cookieButton; // クッキーボタンを追加
+
+        public event Action OnCookieButtonClick; // イベントを追加
+
+        private void Start()
+        {
+            cookieButton.onClick.AddListener(() => OnCookieButtonClick?.Invoke());
+        }
+
+        public void UpdateTotalCookies(decimal totalCookies)
+        {
+            totalCookiesText.text = $"Total Cookies: {totalCookies}";
+        }
+    }
+}
