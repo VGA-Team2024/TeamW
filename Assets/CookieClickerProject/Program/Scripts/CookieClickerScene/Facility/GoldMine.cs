d9e0ae24e43da3cd40ec376d4e1b62d19b4b3428
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-02-16 23:36:49 +0900

    「Update」施設の鉱山のスプリクトの中身を書きました。

--- Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/Facility/GoldMine.cs
+++ Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/Facility/GoldMine.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class GoldMine : MonoBehaviour
 {
     int CurrentGold = 0;
     int MaxGold = 10000;
-    [SerializeField] int GoldPlus;
+    [SerializeField] int GoldPlus = 10;
+    float _timer = 0;
+    int _timePlus = 1;
+    Text _GoldText = null;
 
     private void Start()
     {
-        
+        _GoldText = GetComponent<Text>();
     }
     private void Update()
     {
+        _timer += Time.deltaTime;
+        if (_timer < _timePlus && CurrentGold > MaxGold)
+        {
+            CurrentGold += GoldPlus;
+            _timer = 0;
+            
+        }
+        else
+        {
+            _timer = 0;
+        }
         
     }
     private void OnMouseDown()
     {
-        
+        Debug.Log("‰ñŽû");
     }
+   
 }
