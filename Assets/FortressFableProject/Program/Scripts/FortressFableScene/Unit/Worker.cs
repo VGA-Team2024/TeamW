d3e579da2ed0c0b2dff6ac34b8fdf1028d327592
Shigema <tsubakyun4869@gmail.com>
2024-02-17 15:33:32 +0900

    [add] ファイルとスクリプト（Worker、WarManager、IWar）

---
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/Unit/Worker.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+/// <summary>
+/// 工員のクラス
+/// 最初に1人かならずいる。
+/// 施設の建設時に建設予定地に移動し、建物を建設する。
+/// マップを適当に徘徊してもよい。
+/// </summary>
+public class Worker : UnitBase
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public override void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Action()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/War/WarManager.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+
+/// <summary>
+/// 戦争の機能を持つ
+/// ウェーブ：NEXT
+/// </summary>
+public class WarManager : MonoBehaviour, IWar
+{
+    [Header("現在のウェーブ"), SerializeField] [Tooltip("現在のウェーブ")]
+    int _currentWave;
+
+    [Header("勝ったか"), SerializeField] [Tooltip("勝ったか")]
+    bool _isWin;
+
+    [Header("敵の戦力"), SerializeField] [Tooltip("敵の戦力")]
+    int _enemyForce;
+
+    [Header("味方の戦力"), SerializeField] [Tooltip("味方の戦力")]
+    int _myForce;
+
+    [Header("現在のゴールド"), SerializeField] [Tooltip("現在のゴールド")]
+    int _gold;
+
+
+    #region プロパティ
+
+    /// <summary> 現在のウェーブ </summary>
+    public int CurrentWave
+    {
+        get => _currentWave;
+        //set => _currentWave = value;
+    }
+
+    /// <summary> 勝ったか </summary>
+    public bool IsWin
+    {
+        get => _isWin;
+        //set => _isWin = value;
+    }
+
+    #endregion
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    public void StartWave()
+    {
+        _isWin = false;
+        // ここでenemyForceとmyForceを取得
+    }
+
+    public void EndWave()
+    {
+        ReduceSoldiersAndGold(Comparison());
+    }
+
+    /// <summary>
+    /// NEXTに表示されている兵力 vs 自分が持っている兵士の数(兵力)
+    /// 自分の兵力が高ければ 戦争に勝利 
+    /// </summary>
+    /// <returns></returns>
+    bool Comparison()
+    {
+        if (_myForce >= _enemyForce)
+        {
+            // 次のウェーブ
+            _currentWave++;
+            return _isWin = true;
+        }
+        else
+        {
+            Debug.LogWarning("We lost.");
+            return _isWin = false;
+        }
+    }
+
+    /// <summary>
+    /// 兵士の数を減らす
+    /// 負けた時はゴールドも減らす
+    /// </summary>
+    void ReduceSoldiersAndGold(bool isWin)
+    {
+        if (isWin)
+        {
+            // 切り捨て
+            _myForce -= _enemyForce / 2;
+        }
+        else
+        {
+            // 負けたら
+            _myForce = 0;
+            _gold -= (_enemyForce - _myForce) * 10;
+        }
+
+        if (_gold < 0)
+        {
+            _gold = 0;
+        }
+    }
+}
