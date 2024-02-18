6395e2dad234ee481b7d9128d1807cc39306cbfe
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2024-02-18 22:11:02 +0900

    「Update」鉱山のスプリクトのクリックしたらゴールドをゼロにするコード追加しました。（再度投稿）

--- Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/Facility/GoldMine.cs
+++ Assets/CookieClickerProject/Program/Scripts/CookieClickerScene/Facility/GoldMine.cs
@@ -2,41 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class GoldMine : MonoBehaviour
 {
+
    public int CurrentGold = 0;
     int MaxGold = 10000;
     [SerializeField] int GoldPlus = 10;
-    float _timer = 0;
     int _timePlus = 1;
     Text _GoldText = null;
+    int _currentGold = 0;
+    int _maxGold = 10000;
+    [SerializeField]  int _goldPlus = 10;
+    float _timer = 0;
+    Text _goldText = null;
 
     private void Start()
     {
-        _GoldText = GetComponent<Text>();
+        _goldText = GetComponent<Text>();
     }
+
     private void Update()
     {
         _timer += Time.deltaTime;
-        if (_timer < _timePlus && CurrentGold > MaxGold)
+        if (_timer < _timePlus && _currentGold > _maxGold)
         {
-            CurrentGold += GoldPlus;
+            _currentGold += _goldPlus;
             _timer = 0;
-            
         }
         else
         {
             _timer = 0;
         }
-        
     }
+
     private void OnMouseDown()
     {
+
         Debug.Log("‰ñû");
-        // Gold‚Ì‡Œv•Ï”‚É{‚·‚éG
-        CurrentGold = 0;
+        // Gold‚Ì‡Œv•Ï”‚É{‚·‚é
+        _currentGold = 0;
     }
-   
 }
