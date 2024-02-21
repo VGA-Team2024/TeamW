8276439dcba8af0c2b9f8a2ab35faa6a5f4bc1c3
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-02-21 20:10:59 +0900

    [Add] GameManager

---
+++ Assets/FortressFableProject/Program/Scripts/Common/Core/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CookieClickerProject.Common;
+using CookieClickerProject.Data;
+using UnityEngine;
+
+namespace FortressFableProject.Program.Scripts.Common.Core
+{
+    public class GameManager: AbstractSingleton<GameManager>
+    {
+        public DataStorage dataStorage;
+
+        private void Start()
+        {
+            dataStorage = ScriptableObject.CreateInstance<DataStorage>();
+        }
+    }
+}
