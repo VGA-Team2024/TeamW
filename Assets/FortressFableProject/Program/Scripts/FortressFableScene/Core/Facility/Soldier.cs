7add57ee3776345a423276716fecea43cba1fede
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-02-21 15:44:53 +0900

    [Fix] Folder修正

---
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/Core/Facility/Soldier.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soldiers : MonoBehaviour
+{
+    int _IncreaseSoldiers = 1;
+    int _Dexresesgold = 100;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("生産");
+        /*
+         * += _Dexresesgold;
+         * -= _IncreaseSoldiers;
+         */
+    }
+}
