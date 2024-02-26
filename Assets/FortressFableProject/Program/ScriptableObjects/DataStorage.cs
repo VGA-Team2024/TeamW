f23acd1e057be0270bdf58c6b3baccf02f6105dc
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-02-27 06:26:46 +0900

    [Add] SaveAndLoad

--- Assets/FortressFableProject/Program/ScriptableObjects/DataStorage.cs
+++ Assets/FortressFableProject/Program/ScriptableObjects/DataStorage.cs
@@ -16,36 +16,5 @@ namespace CookieClickerProject.Data
     {
         public PlayerData playerData;
         public GameData gameData;
-
-        private void OnEnable()
-        {
-            playerData = ScriptableObject.CreateInstance<PlayerData>();
-            gameData = ScriptableObject.CreateInstance<GameData>();
-            LoadGame();
-        }
-
-        public void SaveGame()
-        {
-            SaveData saveData = new SaveData
-            {
-                playerData = this.playerData,
-                gameData = this.gameData
-            };
-            string data = JsonUtility.ToJson(saveData, true);
-            string filePath = Path.Combine(Application.persistentDataPath, "gameSave.json");
-            File.WriteAllText(filePath, data);
-        }
-
-        public void LoadGame()
-        {
-            string filePath = Path.Combine(Application.persistentDataPath, "gameSave.json");
-            if (File.Exists(filePath))
-            {
-                string data = File.ReadAllText(filePath);
-                SaveData saveData = JsonUtility.FromJson<SaveData>(data);
-                JsonUtility.FromJsonOverwrite(JsonUtility.ToJson(saveData.playerData), playerData);
-                JsonUtility.FromJsonOverwrite(JsonUtility.ToJson(saveData.gameData), gameData);
-            }
-        }
     }
 }
---
+++ Assets/FortressFableProject/Program/Scripts/Common/Core/SaveAndLoad.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using CookieClickerProject.Data;
+using UnityEngine;
+
+namespace FortressFableProject.Common
+{
+    public class SaveAndLoad : MonoBehaviour
+    {
+        public DataStorage dataStorage;
+
+        private void OnEnable()
+        {
+            dataStorage = ScriptableObject.CreateInstance<DataStorage>();
+            LoadGame();
+        }
+
+        public void SaveGame()
+        {
+            SaveData saveData = new SaveData
+            {
+                playerData = dataStorage.playerData,
+                gameData = dataStorage.gameData
+            };
+            string data = JsonUtility.ToJson(saveData, true);
+            string filePath = Path.Combine(Application.persistentDataPath, "gameSave.json");
+            File.WriteAllText(filePath, data);
+        }
+
+        public void LoadGame()
+        {
+            string filePath = Path.Combine(Application.persistentDataPath, "gameSave.json");
+            if (!File.Exists(filePath)) return;
+            string data = File.ReadAllText(filePath);
+            SaveData saveData = JsonUtility.FromJson<SaveData>(data);
+            JsonUtility.FromJsonOverwrite(JsonUtility.ToJson(saveData.playerData), dataStorage.playerData);
+            JsonUtility.FromJsonOverwrite(JsonUtility.ToJson(saveData.gameData), dataStorage.gameData);
+        }
+    }
+}
