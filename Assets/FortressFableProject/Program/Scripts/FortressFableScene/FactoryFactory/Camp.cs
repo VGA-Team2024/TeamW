8efdc14b563735a2d1ea63275f90cc38632a15d6
Shigema <tsubakyun4869@gmail.com>
2024-02-27 15:21:56 +0900

    [add] 施設を生成する処理

---
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/FactoryFactory/Camp.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// キャンプ
+/// </summary>
+public class Camp : FactoryBase
+{
+    public override void Effect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
