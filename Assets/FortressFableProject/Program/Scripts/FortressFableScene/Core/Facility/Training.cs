e2154386289504f1d1d72d37835605f55dff84f1
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-02-29 00:35:03 +0900

    [Upadte]

--- Assets/FortressFableProject/Program/Scripts/FortressFableScene/Core/Facility/Training.cs
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/Core/Facility/Training.cs
@@ -1,11 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class youseizyo: MonoBehaviour
+public class Training : FacilityBase
 {
     int _IncreaseSoldiers = 1;
     int _Dexresesgold = 100;
+
+    float _timer = 0f;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer < 10f)
+        {
+            UnitManager.Instance.CreateSoldier();
+        }
+    }
 
     private void OnMouseDown()
     {
--- Assets/FortressFableProject/Program/Scripts/FortressFableScene/Unit/UnitBase.cs
+++ Assets/FortressFableProject/Program/Scripts/FortressFableScene/Unit/UnitBase.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 [System.Serializable]
 public abstract class UnitBase : MonoBehaviour, IUnit
 {
-    public UnitType Type { get; set; } //ユニットの種類
-    public int Count { get; set; } //ユニットの数
+    [field: SerializeField] public UnitType Type { get; set; } //ユニットの種類
+    [field: SerializeField] public int Count { get; set; } //ユニットの数
 
     public enum UnitType
     {
